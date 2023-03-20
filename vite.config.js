22c1cd97fbf6350d3776a3629005ba25ea2e41fd
rossreicks <80704623+rossreicks@users.noreply.github.com>
2023-03-19 20:11:26 -0500

    test: finish test code coverage (#8)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        coverage: {
+            exclude: [...configDefaults.coverage.exclude, 'lib/*'],
+        },
         globals: true,
     },
 });
